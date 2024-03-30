b9158dcaa13f7f1d55a46472ef0536729327ada5
Mouchi0357 <60027874+kouke0638@users.noreply.github.com>
2024-03-31 03:26:09 +0800

    Add files via upload

---
+++ Windows Shutdown Scheduler.bat
@@ -0,0 +1,22 @@
+@echo off
+set /p hours=Enter number of hours: 
+set /p minutes=Enter number of minutes: 
+set /p seconds=Enter number of seconds:
+
+:: Calculate total seconds
+set /a total_seconds=(hours*3600)+(minutes*60)+seconds
+
+echo The scheduled shutdown time is %hours% hours %minutes% minutes %seconds% seconds from now.
+
+:: Calculate total minutes
+set /a total_minutes=total_seconds/60
+echo Coputer will shutdown in %total_minutes% minutes.
+
+:: Confirm to proceed
+set /p confirm=Are you sure you want to shutdown after the specified time? (Y/N):
+if /i "%confirm%" neq "Y" goto end
+
+:: Execute scheduled shutdown
+shutdown -s -t %total_seconds%
+
+:end
